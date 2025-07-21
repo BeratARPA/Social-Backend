007e9599728ed92bade7748192dcd7ce6492950a
Berat ARPA <beratarpa@hotmail.com>
2025-07-21 21:37:00 +0300

    AuthService Data Düzenlemesi.

--- src/Modules/AuthService/Data/AuthDbContext.cs
+++ src/Modules/AuthService/Data/AuthDbContext.cs
@@ -1,8 +1,50 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AuthService.Data.Entities;
+using AuthService.Data.UnitOfWork;
+using AuthService.Dtos;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuthService.Data
 {
-    public class AuthDbContext : DbContext
+    public class AuthDbContext : DbContext, IUnitOfWork
     {
+        private readonly IMediator _mediator;
+
+        public AuthDbContext() : base() { }
+
+        public AuthDbContext(DbContextOptions<AuthDbContext> options, IMediator mediator)
+            : base(options)
+        {
+            _mediator = mediator;
+        }
+
+        public DbSet<UserCredential> UserCredentials { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+        public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+        {
+            await base.SaveChangesAsync(cancellationToken);
+
+            var domainEntities = ChangeTracker
+             .Entries<BaseEntity>()
+             .Where(e => e.Entity.DomainEvents != null && e.Entity.DomainEvents.Any())
+             .ToList();
+
+            var domainEvents = domainEntities
+                .SelectMany(e => e.Entity.DomainEvents)
+                .ToList();
+
+            domainEntities.ForEach(entity => entity.Entity.ClearDomainEvents());
+
+            foreach (var domainEvent in domainEvents)
+                await _mediator.Publish(domainEvent);
+
+            return true;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new UserEntityConfigurations());
+        }
     }
 }
---
+++ src/Modules/AuthService/Data/Repositories/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using AuthService.Data.UnitOfWork;
+
+namespace AuthService.Data.Repositories
+{
+    public interface IRepository<T>
+    {
+        IUnitOfWork UnitOfWork { get; }
+    }
+}
